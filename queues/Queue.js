0990c6e26a227ea68e606fffb468f7d0da7dcfe0
justinrasler <85715506+justinrasler@users.noreply.github.com>
2023-01-04 14:16:10 -0500

    queue practice

--- queues/Queue.js
+++ queues/Queue.js
@@ -1,23 +1,53 @@
-const LinkedList = require('./LinkedList');
+const LinkedList = require("./LinkedList");
 
 class Queue {
-  constructor() {
+  constructor(maxSize = Infinity) {
     this.queue = new LinkedList();
+    this.maxSize = maxSize;
     this.size = 0;
   }
-  enqueue(data) {
-  this.queue.addToTail(data);
-  this.size ++
- console.log(`Added ${data}! queue size is now ${this.size}`)
-  }
-  } 
 
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  enqueue(data) {
+    if (this.hasRoom()) {
+      this.queue.addToTail(data);
+      this.size++;
+      console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+    } else {
+      throw new Error("Queue is full!");
+    }
+  }
+
+  dequeue() {
+    if (!this.isEmpty()) {
+      const data = this.queue.removeHead();
+      this.size--;
+      console.log(
+        `Removed ${data} from queue! Queue size is now ${this.size}.`
+      );
+      return data;
+    } else {
+      throw new Error("Queue is empty!");
+    }
+  }
+}
 
 module.exports = Queue;
 
-const restaurantOrder = new Queue();
-console.log(`restaurantOrder queue has ${restaurantOrder.size} orders.\n`);
-restaurantOrder.enqueue('apple pie');
-restaurantOrder.enqueue('roast chicken');
-restaurantOrder.enqueue('quinoa salad');
+const boundedQueue = new Queue(3);
 
+boundedQueue.enqueue(1);
+boundedQueue.enqueue(2);
+boundedQueue.enqueue(3);
+
+boundedQueue.dequeue();
+boundedQueue.dequeue();
+boundedQueue.dequeue();
+boundedQueue.dequeue();
